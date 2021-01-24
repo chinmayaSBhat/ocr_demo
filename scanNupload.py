e64d78b2222667d85949010471a848a5227dea53
chinmayaSBhat <44517358+chinmayaSBhat@users.noreply.github.com>
2021-01-24 15:04:38 +0530

    Update scanNupload.py

--- scanNupload.py
+++ scanNupload.py
@@ -82,8 +82,8 @@ def edit_results():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    application.debug=True
-    application.run(host='0.0.0.0')
+    #application.debug=True
+    application.run()
 
 
 
